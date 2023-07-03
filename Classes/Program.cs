c4b86e76f7a0e86c063fbf499a6f189e73f2485a
derrickdrye <dryd95@hotmail.com>
2023-07-03 15:48:47 -0500

    ClassesExerciseComplete

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "Accord";
+            myCar.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "626",
+                Year = 2001
+            };
+
+            Car nissan = new Car("Nissan", "Maxima", 2022);
+
+            var carList = new List<Car>() { myCar, mazda, nissan };
+            
+            Console.WriteLine(carList.Capacity);
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+            
+
+
+
         }
     }
 }
